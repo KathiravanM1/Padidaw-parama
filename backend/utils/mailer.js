fb00ba9863d0795c2d8189f97e6427e5c559fa70
Honey-Joe <honeyjoe943@gmail.com>
2026-09-16 22:29:18 +0530

    Updated the Repo

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -3,4 +3,4 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendMail = ({ to, subject, html }) =>
-  resend.emails.send({ from: 'Vidivu <onboarding@resend.dev>', to, subject, html });
+  resend.emails.send({ from: process.env.MAIL_FROM, to, subject, html });
